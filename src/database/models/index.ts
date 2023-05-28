8915c7625561acadc4bf6d320362a2a9d807934d
jrbigmon <juniorterriaga0@gmail.com>
2023-05-27 21:48:39 -0300

    feat: adjusted get model

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,19 +1,23 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+import fs from 'fs';
+import path from 'path';
+import * as Sequelize from 'sequelize';
+import process from 'process';
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.ts')[env];
-const db = {};
+const db = {
+  sequelize: undefined,
+  Sequelize: undefined,
+};
 
-let sequelize;
+let sequelize: Sequelize.Sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize.Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize.Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
