61b48de948b89b41850275a60a1268154ebab99d
jrbigmon <juniorterriaga0@gmail.com>
2023-05-28 00:25:06 -0300

    fix: adjusted migrations

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     port: process.env.DB_PORT,
   },
 }
